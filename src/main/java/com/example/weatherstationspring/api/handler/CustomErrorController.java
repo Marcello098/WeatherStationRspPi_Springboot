a73e08d314a687126df640a1b0c023bae1768480
Marcello098 <marcel0453@wp.pl>
2023-02-22 12:20:47 +0100

    Added some basic error response handling

---
+++ src/main/java/com/example/weatherstationspring/api/handler/CustomErrorController.java
@@ -0,0 +1,34 @@
+package com.example.weatherstationspring.api.handler;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/error")
+@Controller
+public class CustomErrorController implements ErrorController {
+
+
+    @GetMapping
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404.html";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500.html";
+            }
+        }
+        return "error.html";
+    }
+
+}
