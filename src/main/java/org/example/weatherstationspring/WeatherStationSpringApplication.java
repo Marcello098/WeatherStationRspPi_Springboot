bc1d0b1d2b7d2ccd15d7b6a92290b62d05b745e9
Kamil00o <laganowski.kamil@gmail.com>
2023-02-24 02:22:59 +0100

    Integration with scd project (GUI)
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- src/main/java/org/example/weatherstationspring/WeatherStationSpringApplication.java
+++ src/main/java/org/example/weatherstationspring/WeatherStationSpringApplication.java
@@ -1,13 +1,20 @@
 package org.example.weatherstationspring;
 
+
+/*import org.example.DataContainer;
+import org.example.GUI;*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.swing.*;
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableScheduling
 public class WeatherStationSpringApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(WeatherStationSpringApplication.class, args);
+        //GUI weatherRspPiGUI = new GUI(new DataContainer());
     }
 }
